16975b0b6a0135a2071347a20bc6fc5c58d44a7f
Thomas <thomas-lm@vikenfiber.no>
2014-06-24 19:05:06 +0200

    Update server.js
    hihi

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    // 200 betyr ok på internett (eller, http, da)
+    // 200 betyr ok pï¿½ internett (eller, http, da)
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hei \n');
+    res.end('Thomas! \n');
 }).listen(process.env.port);
