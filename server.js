c03114b5862bd5d947e0315dca591ac679e7e74d
Rekonion1 <robinsorlien@gmail.com>
2014-06-24 19:16:29 +0200

    Update server.js
    Bare legg dette til, for moro skyld

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     // 200 betyr ok p� internett (eller, http, da)
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('PEACE \n');
+    res.end('This website is pointless FOR NOW \n');
 }).listen(process.env.port);
