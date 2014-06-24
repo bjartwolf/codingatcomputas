5a298d670065dfdc961f768bd07f4707c3045f14
Mathiashelseth99 <mathiashelseth@yahoo.no>
2014-06-24 19:10:06 +0200

    Update server.js
    Vet ikke om dette funker...?

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    // 200 betyr ok på internett (eller, http, da)
+    // 200 betyr ok pï¿½ internett (eller, http, da)
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hei \n');
 }).listen(process.env.port);
+
+<FORM NAME="myform" ACTION="" METHOD="GET">
+Enter something in the box: <BR>
+<INPUT TYPE="text" NAME="inputbox" VALUE=""><P>
+<INPUT TYPE="button" NAME="button" Value="Click" onClick="testResults(this.form)">
+</FORM>
